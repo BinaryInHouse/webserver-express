2fa0e68783de59518b08635fb8ef81cac9c5807d
Alex Meregildo <alexmeco95@gmail.com>
2018-10-14 01:15:41 -0500

    Mi primer commit

---
+++ server.js
@@ -0,0 +1,35 @@
+var express = require('express')
+var app = express()
+
+const hbs = require('hbs');
+require('./hbs/helpers');
+
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public')); //Mildware
+
+
+//Express HBS engine para paginas dinamicas
+hbs.registerPartials(__dirname + '/views/parciales');
+app.set('view engine', 'hbs');
+
+
+app.get('/', (req, res) => {
+
+    res.render('home', {
+        nombre: 'Alex meregildo',
+        anio: new Date().getFullYear()
+    });
+});
+
+app.get('/about', (req, res) => {
+
+    res.render('about', {
+        anio: new Date().getFullYear()
+    });
+});
+
+app.listen(3000, () => {
+
+    console.log(`Escuchando peticiones en el puerto ${port}`);
+});
